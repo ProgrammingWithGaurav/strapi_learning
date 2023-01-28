4b61a34a87318dc468dbc8aadcde7363949c3e89
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2023-01-28 16:53:03 +0530

    removing warnings

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -17,7 +17,6 @@ export default function Home() {
             <div className="flex flex-col text-center w-full mb-20">
               <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Start Shopping</h2>
               <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 hover:bg-gray-50 hover:text-gray-700 transition cursor-pointer rounded-md" onClick={() => router.push('/products')}>Start Shopping</h1>
-              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
             </div>
             <div className="flex flex-wrap">
               <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
